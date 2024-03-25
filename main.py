ea9f4a6d85bf8b0d86b0b20121c6a665763ca60e
U Shin <55006699+ushin-cse@users.noreply.github.com>
2024-03-25 18:56:46 -0400

    Update main.py

--- main.py
+++ main.py
@@ -10,14 +10,11 @@ resources.
 
 import eel
 import serial_com
-#import rfcontroller # This import should be commented out when testing on a PC
 import json
 import pyautogui
-import threading
-#import pyttsx3
 from gtts import gTTS
 import os
-from serial_com import send_command, TVCommand  # Import the send_command function and TVCommand enum from serial_com.py
+from serial_com import send_command, TVCommand
 
 def powerOnOff():
     """Send power on command."""
@@ -43,30 +40,26 @@ def channelDown():
     """Send channel down command."""
     serial_com.send_command(serial_com.TVCommand.CHANNEL_DOWN.value)
 
-#controller = rfcontroller.RFController() # Comment this out when developing on desktop
 screenWidth, screenHeight = pyautogui.size()
 pyautogui.FAILSAFE = False
 
 def togglePlug(command):
     """Toggle plug."""
-    print(command) # For testing on a PC, this line should be uncommented, and the following line should be commented out
-    #controller.sendcode(command) # This line should be commented out when testing on PC, the library is not available on PC
+    print(command)
+    # controller.sendcode(command)
 
 def storeConfig(setting, value):
     """Store configuration."""
     config = {}
     with open('config.json', 'r') as openfile:
-    #with open('/home/pi/ALS-Assistive-Tech/config.json', 'r') as openfile:
-        config  = json.load(openfile)
+        config = json.load(openfile)
     with open('config.json', 'w') as writefile:
-    #with open('/home/pi/ALS-Assistive-Techp/config.json', 'w') as writefile:
         config[setting] = value
         json.dump(config, writefile)
 
 def loadConfig():
     """Load configuration."""
     with open('config.json', 'r') as openfile:
-    #with open('/home/pi/ALS-Assistive-Tech/config.json', 'r') as openfile:
         config = json.load(openfile)
         eel.loadConfig(config)
 
@@ -76,10 +69,10 @@ def resetMouse():
 
 def speak_from_text(text):
     """Speak from text."""
-    speak_text(text)  # The function defined above
+    speak_text(text)
 
 def cleanup():
-    """Cleanup resources."""
+    """Clean up resources."""
     serial_com.ser.close()
     print("Serial port closed")
 
